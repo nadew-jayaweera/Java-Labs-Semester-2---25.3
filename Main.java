61dd0113971a6148b71207da3c8f8491863ebd04
nadew-jayaweera <nadew.256@gmail.com>
2026-04-28 09:12:35 +0530

    .

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter first Number: ");
+
+        int num1 = sc.nextInt();
+        
+        System.out.println("Enter second Number: ");
+        int num2 = sc.nextInt();
+    }
+}
